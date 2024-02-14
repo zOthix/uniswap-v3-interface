669742f83d513fe2e310ae9c0993ab873712f2de
usman Pervaiz <usmanpervaiz724@gmail.com>
2024-02-14 17:50:55 +0500

    fix pool button colors

--- apps/web/src/components/Button/index.tsx
+++ apps/web/src/components/Button/index.tsx
@@ -97,8 +97,10 @@ export const ButtonPrimary = styled(BaseButton)`
   }
 
   background: #6E42CA;
-  :hover {
-    background-color: #6d42cab3;
+  &:hover ,  &:active , &:focus {
+    background-color: #6d42cab3 !important;
+    box-shadow: 0 0 0 1pt ${({ theme }) => darken(0.1, "#6E42CA")};
+    border: 1px solid "#6E42CA";
   }
 
 `
